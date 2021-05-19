cbbca8684adbfc67ebcb66b54fca3a5c05535e90
Miguel Angel <leonaguirrem3@gmail.com>
2021-05-19 16:25:44 -0500

    Commit: Se modificó el codigo para listar archivos de la pc local

---
+++ Client/src/client/ClientOperation.java
@@ -0,0 +1,35 @@
+package client;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
+import rmiinterface.RMIInterface;
+
+public class ClientOperation {
+    private static RMIInterface look_up;
+
+    public static void main(String[] args)
+            throws MalformedURLException, RemoteException, NotBoundException {
+
+        look_up = (RMIInterface) Naming.lookup("//localhost/MyServer");
+        String ruta = JOptionPane.showInputDialog("Que ruta mostrar");
+
+        //String response = look_up.helloTo(txt);
+        ArrayList<String> response = look_up.obtenerArchivos(ruta);
+
+        Iterator<String> Iterator = response.iterator();
+        while(Iterator.hasNext()){
+            String elemento = Iterator.next();
+            System.out.println(elemento);
+        }
+        //System.out.println(response);
+        //JOptionPane.showMessageDialog(null, response);
+
+    }
+}
