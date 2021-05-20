93c416e59ce70359d47796770711d9a79ce45c17
Miguel Angel <leonaguirrem3@gmail.com>
2021-05-20 08:25:27 -0500

    Commit: Se agregó un archivo para generar un reporte de los archivos de una PC remota

--- Client/src/client/ClientOperation.java
+++ Client/src/client/ClientOperation.java
@@ -1,28 +1,31 @@
 package client;
 
-import java.net.MalformedURLException;
+import com.itextpdf.text.DocumentException;
+import rmiinterface.RMIInterface;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-import javax.swing.JOptionPane;
-
-import rmiinterface.RMIInterface;
 
 public class ClientOperation {
     private static RMIInterface look_up;
 
     public static void main(String[] args)
-            throws MalformedURLException, RemoteException, NotBoundException {
+            throws IOException, NotBoundException, DocumentException {
 
         look_up = (RMIInterface) Naming.lookup("//85.187.158.121:1099/MyServer");
-        String ruta = JOptionPane.showInputDialog("¿Que ruta mostrar?");
+        String ruta = JOptionPane.showInputDialog("Que ruta mostrar");
+
+        // Crear objeto de GeneratePDF
+        GeneratePDF pdf = new GeneratePDF();
 
         //String response = look_up.helloTo(txt);
         ArrayList<String> response = look_up.obtenerArchivos(ruta);
-
+        pdf.crearPDF(new File("Reporte.pdf"), response, ruta);
         Iterator<String> Iterator = response.iterator();
         while(Iterator.hasNext()){
             String elemento = Iterator.next();
@@ -33,3 +36,4 @@ public class ClientOperation {
 
     }
 }
+
